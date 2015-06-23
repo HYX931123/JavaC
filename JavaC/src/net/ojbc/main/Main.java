00eeb8f41f0689637253cf60836bbae2e1cffbda
HYX931123 <15517551511@126.com>
2015-06-23 13:14:26 +0800

    Java测试类

---
+++ JavaC/src/net/ojbc/main/Main.java
@@ -0,0 +1,18 @@
+package net.ojbc.main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		int b = 0;
+		a--;
+		b++;
+
+		System.out.println("a:"+a+" ,b:"+b);
+	}
+
+}
